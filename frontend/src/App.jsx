fa5fca4c4acd59848e0305a425ea47d1e026904f
TuhinCodeCraft <tuhinghosh0169@gmail.com>
2024-08-21 10:15:30 +0530

    completed the styling part in our application

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import Login from './pages/login/Login'
 import SignUp from './pages/signup/SignUp'
+import Home from './pages/home/Home'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,7 +9,8 @@ function App() {
   return (
     <div className='p-4 h-screen flex items-center justify-center'>
         {/* <Login /> */}
-        <SignUp/>
+        {/* <SignUp/> */}
+        <Home/>
     </div>
   )
 }
--- frontend/src/pages/signup/GenderCheckbox.jsx
+++ frontend/src/pages/signup/GenderCheckbox.jsx
@@ -3,14 +3,14 @@ const GenderCheckbox = () => {
 		<div className='flex'>
 			<div className='form-control'>
 				<label className={`label gap-2 cursor-pointer`}>
+					<input type='checkbox' className='checkbox border-slate-100' />
 					<span className='label-text'>Male</span>
-					<input type='checkbox' className='checkbox border-slate-900' />
 				</label>
 			</div>
 			<div className='form-control'>
 				<label className={`label gap-2 cursor-pointer`}>
+					<input type='checkbox' className='checkbox border-slate-100' />
 					<span className='label-text'>Female</span>
-					<input type='checkbox' className='checkbox border-slate-900' />
 				</label>
 			</div>
 		</div>
