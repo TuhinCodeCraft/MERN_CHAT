8893ec281b208e4e0c157334f4f8c9ae81cab645
TuhinCodeCraft <tuhinghosh0169@gmail.com>
2024-08-23 22:21:38 +0530

    completed the login and logout functionality

--- frontend/src/components/sidebar/LogoutButton.jsx
+++ frontend/src/components/sidebar/LogoutButton.jsx
@@ -1,17 +1,18 @@
 import { CgLogOut } from "react-icons/cg";
-// import useLogout from "../../hooks/useLogout";
+import useLogout from "../../hooks/useLogout";
+
 
 const LogoutButton = () => {
-	// const { loading, logout } = useLogout();
+	const { loading, logout } = useLogout();
 
 	return (
 		<div className='mt-auto'>
-            <CgLogOut className='w-6 h-6 text-white cursor-pointer' />
-			{/* {!loading ? (
-				<BiLogOut className='w-6 h-6 text-white cursor-pointer' onClick={logout} />
+            
+			{!loading ? (
+				<CgLogOut className='w-6 h-6 text-white cursor-pointer' onClick={logout} />
 			) : (
 				<span className='loading loading-spinner'></span>
-			)} */}
+			)}
 		</div>
 	);
 };
